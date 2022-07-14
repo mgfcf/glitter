db74b0784104c3d8ad6a51106e0bdb243c1ecd76
Max <m.giller.dev@gmail.com>
2022-07-15 00:12:36 +0200

    Removes placeholders and initial custom pages

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from '@/components/header.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <Header />
   <RouterView />
 </template>
 
 <style>
 @import "@/assets/base.css";
 
-#app {
+/* #app {
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
@@ -116,4 +104,6 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
+
+*/
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <p>Content under construction</p>
+  </main>
+</template>
