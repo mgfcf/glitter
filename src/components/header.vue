b47b7ffc68d26fd55a83326de37da29c8043ecfb
Max <m.giller.dev@gmail.com>
2022-07-16 22:26:07 +0200

    Simple header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,10 +1,13 @@
 <template>
-    <header class="navigation">
-        <router-link class="button" to="/memos">Memos</router-link>
-        <router-link class="button" to="/essays">Essay</router-link>
-        <router-link class="button" to="/about">About</router-link>
+    <header>
         <router-link to="/"><img alt="Giller" class="logo" src="@/assets/giller-logo.svg" width="125" height="125" />
         </router-link>
+        <div class="navigation">
+
+            <router-link to="/memos">Memos</router-link>
+            <router-link to="/essays">Essay</router-link>
+            <router-link to="/about">About</router-link>
+        </div>
     </header>
 </template>
 
@@ -14,42 +17,21 @@
 <style scoped>
 
 header {
-    text-align: right;
+    text-align: center;
 }
 
 .navigation {
-    list-style-type: none;
     margin: 0;
     padding: 0;
-    display: inline;
-    line-height: 6;
-}
-
-.navigation *.button {
-    display: inline-block;
-    padding: 1rem;
-
-    vertical-align: top;
 }
 
 .navigation * {
+    display: inline-block;
+    padding: 1rem;
     margin: 0rem 1rem;
 }
 
 .navigation *:hover {
     text-decoration: underline;
-    cursor: default;
-    -webkit-user-select: none;
-    /* Safari */
-    -moz-user-select: none;
-    /* Firefox */
-    -ms-user-select: none;
-    /* IE10+/Edge */
-    user-select: none;
-    /* Standard */
-}
-
-.logo {
-    display: inline;
 }
 </style>
