7d848872702ec60ee004f4e8f9a171212156537b
Max <m.giller.dev@gmail.com>
2022-07-16 22:56:53 +0200

    Renamed import

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import Header from '@/components/header.vue'
+import Header from '@/components/Header.vue'
 </script>
 
 <template>
